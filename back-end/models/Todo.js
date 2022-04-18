0a80c2887cf4eef8571c28d05ba18f600c54ee9f
Mstars_lab1_07 <mstars_lab1_07@Mstars-lab1-07s-iMac.local>
2022-04-18 18:40:11 +0800

    TodoSchema uurchluv

--- back-end/models/Todo.js
+++ back-end/models/Todo.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
+    todoText: {
+        type: String
+    },
     createdAt: {
         type: Date,
         default: Date.now
